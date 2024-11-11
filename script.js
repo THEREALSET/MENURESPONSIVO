e403d23a686cacffc47faaae1fad232fc783b124
THEREALSET <seltonluiz007@gmail.com>
2024-11-11 18:26:20 -0300

    att

---
+++ script.js
@@ -0,0 +1,8 @@
+let men = document.getElementById('men')
+function menu() {
+	if (men.style.display == `block`) {
+        men.style.display = `none`
+    } else {
+        men.style.display = `block`
+    }
+}
