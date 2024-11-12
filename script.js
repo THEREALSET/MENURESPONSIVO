312a0935127211fe21763493543e9294dd13ed9c
THEREALSET <seltonluiz007@gmail.com>
2024-11-12 18:13:27 -0300

    att

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ function menu() {
         men.style.display = `block`
     }
 }
+
+function altel() {
+    if (window.innerWidth >= 768) {
+        men.style.display = `block`
+    } else {
+        men.style.display = `none`
+    }
+}
